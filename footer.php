dc5755ce7396b1af9b3928c80ae40a328c4a3f67
Jimmy Pocock <jimmypocock@yahoo.com>
2015-10-10 16:29:03 -0500

    Uploading blank theme.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Shweiki_Design
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'shweiki-design' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'shweiki-design' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'shweiki-design' ), 'shweiki-design', '<a href="http://jimmypocock.com" rel="designer">Jimmy Pocock</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
