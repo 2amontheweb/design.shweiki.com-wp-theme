bbb1fb9d3289d28dab4830bbf5fbebcdd2112fe0
Jimmy Pocock <jimmypocock@yahoo.com>
2015-10-10 18:38:31 -0500

    Updated footer and have all the bootstrap and jquery js in there.

--- footer.php
+++ footer.php
@@ -6,21 +6,48 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Shweiki_Design
+ * @package Bootstrap_to_Wordpress
  */
 
 ?>
 
-  <footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="site-info">
-      <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'shweiki-design' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'shweiki-design' ), 'WordPress' ); ?></a>
-      <span class="sep"> | </span>
-      <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'shweiki-design' ), 'shweiki-design', '<a href="http://jimmypocock.com" rel="designer">Jimmy Pocock</a>' ); ?>
-    </div><!-- .site-info -->
-  </footer><!-- #colophon -->
-</div><!-- #page -->
+    <?php wp_footer(); ?>
 
-<?php wp_footer(); ?>
+    <!-- FOOTER
+    ================================================== -->
+    <footer>
+      <div class="container">
+        <div class="col-sm-3">
+          <!-- <p><a href="/"><img src="<?#php bloginfo('stylesheet_directory'); ?>/img/logo.png" alt="Bootstrap to WordPress"></a></p> -->
+        </div><!-- end col -->
+        <div class="col-sm-6">
+
+          <?php
+              wp_nav_menu( array(
+
+                'theme_location' => 'header',
+                'container'       => 'nav',
+                'menu_class'        => 'list-unstyled list-inline'
+
+              ) );
+            ?>
+
+        </div><!-- end col -->
+        <div class="col-sm-3">
+          <p class="pull-right"><?php echo bloginfo('name') ?> &copy; <?php echo date('Y') ?> <?php the_author_link(); ?></p>
+          <?php dynamic_sidebar( 'sidebar-2' ); ?>
+        </div><!-- end col -->
+      </div><!-- container -->
+    </footer>
+  </div><!-- #page -->
+
+
+  <!-- Bootstrap core JavaScript
+  ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="<?php bloginfo('template_directory') ?>/js/jquery-2.1.1.min.js"></script>
+  <script src="<?php bloginfo('template_directory') ?>/js/bootstrap.min.js"></script>
 
 </body>
 </html>
