1bc21e5fc3b2985e7c503c88885b82c5f4ba59fa
Jimmy Pocock <jimmypocock@yahoo.com>
2015-10-11 14:25:41 -0500

    Configured custom post taxonomies. YAY. and got the links on portfolio page working.

--- page-portfolio.php
+++ page-portfolio.php
@@ -45,9 +45,11 @@ get_header(); ?>
 
           ?>
             <div class="resource col-sm-6">
-              <img src="<?php echo $portfolio_image[url]; ?>" alt="<?php echo $portfolio_image[alt]; ?>">
-              <?php the_post_thumbnail(); ?>
-              <h3><a href="<?php echo $portfolio_url; ?>"><?php the_title(); ?></a></h3>
+              <a href="<?php echo esc_url( get_permalink() ); ?>">
+                <img src="<?php echo $portfolio_image[url]; ?>" alt="<?php echo $portfolio_image[alt]; ?>">
+              </a>
+              <?#php the_post_thumbnail(); ?>
+              <?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
               <p><?php the_excerpt(); ?></p>
             </div>
 
