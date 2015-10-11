dca6b11d24758ff63c8fd8989168acce405c72a1
Jimmy Pocock <jimmypocock@yahoo.com>
2015-10-10 23:00:26 -0500

    Got a sort of master slider showing on the main page.

--- page-main.php
+++ page-main.php
@@ -5,6 +5,26 @@
 
 get_header(); ?>
 
-  <h1>Main Page</h1>
+  <!-- MAIN CONTENT
+================================================== -->
+<div class="container page">
+  <div class="row" id="primary">
+
+    <div id="content" class="col-sm-12">
+
+      <section class="main-content">
+
+        <?php while ( have_posts() ) : the_post();  ?>
+
+        <?php the_content(); ?>
+
+        <?php endwhile; ?>
+
+      </section>
+
+    </div><!-- content -->
+
+  </div><!-- primary -->
+</div><!-- container -->
 
 <?php get_footer(); ?>
