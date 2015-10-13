e400f56792812677b1dd55e4f3411d45c43d77c7
Jimmy Pocock <jimmypocock@yahoo.com>
2015-10-12 21:17:37 -0500

    Got the footer lookin' hot.

--- footer.php
+++ footer.php
@@ -20,33 +20,35 @@
         <div class="row">
           <div class="col-sm-4 section footer-left">
             <h3>Contact Us</h3>
-            <img style="width:80%;" src="<?php bloginfo('stylesheet_directory'); ?>/img/gmap.png" alt="GMap">
+            <a href="/contact">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/img/gmap.png" alt="GMap">
+            </a>
           </div><!-- .contact-us -->
           <div class="col-sm-4 section footer-middle">
             <h3>Connect With Us</h3>
             <div class="row">
               <div class="col-xs-3">
-                <a href="http://www.twitter.com">
-                  <img style="max-width:60px;width:90%;display:block;margin: 0 auto;" src="<?php bloginfo('stylesheet_directory'); ?>/img/social/twitter.png" alt="GMap">
+                <a href="http://www.twitter.com" target="_blank">
+                  <img src="<?php bloginfo('stylesheet_directory'); ?>/img/social/twitter.png" alt="GMap">
                 </a>
               </div>
               <div class="col-xs-3">
-                <a href="http://www.twitter.com">
-                  <img style="max-width:60px;width:90%;display:block;margin: 0 auto;" src="<?php bloginfo('stylesheet_directory'); ?>/img/social/instagram.png" alt="GMap">
+                <a href="http://www.twitter.com" target="_blank">
+                  <img src="<?php bloginfo('stylesheet_directory'); ?>/img/social/instagram.png" alt="GMap">
                 </a>
               </div>
               <div class="col-xs-3">
-                <a href="http://www.twitter.com">
-                  <img style="max-width:60px;width:90%;display:block;margin: 0 auto;" src="<?php bloginfo('stylesheet_directory'); ?>/img/social/facebook.png" alt="GMap">
+                <a href="http://www.twitter.com" target="_blank">
+                  <img src="<?php bloginfo('stylesheet_directory'); ?>/img/social/facebook.png" alt="GMap">
                 </a>
               </div>
               <div class="col-xs-3">
-                <a href="http://www.twitter.com">
-                  <img style="max-width:60px;width:90%;display:block;margin: 0 auto;" src="<?php bloginfo('stylesheet_directory'); ?>/img/social/pinterest.png" alt="GMap">
+                <a href="http://www.twitter.com" target="_blank">
+                  <img src="<?php bloginfo('stylesheet_directory'); ?>/img/social/pinterest.png" alt="GMap">
                 </a>
               </div>
             </div>
-            <p>Stay in the loop on what's going on with <div>Shweiki Design &amp; Marketing</div></p>
+            <p>Stay in the loop on what's going on with <span>Shweiki Design &amp; Marketing</span></p>
           </div><!-- .connect-with-us -->
           <div class="col-sm-4 section footer-right">
             <?php dynamic_sidebar( 'footer-right' ); ?>
